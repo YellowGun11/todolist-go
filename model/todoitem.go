99dd9256e01b18804462dadb1462da82e920cc9d
siskinc <susecjh@gmail.com>
2019-09-27 13:36:39 +0800

    add tags of todoitem

--- model/todoitem.go
+++ model/todoitem.go
@@ -5,10 +5,10 @@ import "github.com/globalsign/mgo/bson"
 type TODOItem struct {
 	ID             bson.ObjectId `json:"id" bson:"_id"`
 	OwnerID        bson.ObjectId `json:"owner_id" bson:"owner_id"`
-	TopicID        bson.ObjectId
-	FatherID       bson.ObjectId
-	Content        string
-	Status         uint32
-	CompletionTime uint64
-	DeletedTime    uint64
+	TopicID        bson.ObjectId `json:"topic_id" bson:"topic_id"`
+	FatherID       bson.ObjectId `json:"father_id" bson:"father_id"`
+	Content        string        `json:"content" bson:"content"`
+	Status         uint32        `json:"status" bson:"status"`
+	CompletionTime uint64        `json:"completion_time" bson:"completion_time"`
+	DeletedTime    uint64        `json:"deleted_time" bson:"deleted_time"`
 }
