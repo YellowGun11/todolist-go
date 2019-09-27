113d87d6aa4a35b72beba6c8ddbd79cf4ebca4e1
siskinc <susecjh@gmail.com>
2019-09-27 13:34:36 +0800

    change type of id to bson.ObjectID

--- model/todoitem.go
+++ model/todoitem.go
@@ -1,10 +1,12 @@
 package model
 
+import "github.com/globalsign/mgo/bson"
+
 type TODOItem struct {
-	ID             string
-	OwnerID        string
-	TopicID        string
-	FatherID       string
+	ID             bson.ObjectId `json:"id" bson:"_id"`
+	OwnerID        bson.ObjectId `json:"owner_id" bson:"owner_id"`
+	TopicID        bson.ObjectId
+	FatherID       bson.ObjectId
 	Content        string
 	Status         uint32
 	CompletionTime uint64
